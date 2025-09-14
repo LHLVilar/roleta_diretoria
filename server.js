0e4c2c59c56873e1479fb87d7c357ffd690a98cd
LHLVilar <101405552+LHLVilar@users.noreply.github.com>
2025-09-14 08:16:38 -0300

    Update server.js
    removido código temporário apagar listas

--- server.js
+++ server.js
@@ -17,29 +17,6 @@ const db = new Pool({
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// CÓDIGO TEMPORÁRIO PARA LIMPAR O BANCO DE DADOS
-app.get("/clear-all-lists", async (req, res) => {
-  try {
-    await db.query("DELETE FROM morning_list;");
-    await db.query("DELETE FROM afternoon_list;");
-    await db.query("DELETE FROM morning_draw;");
-    await db.query("DELETE FROM afternoon_draw;");
-    
-    // limpar arrays em memória
-    morningList = [];
-    afternoonList = [];
-    morningDraw = [];
-    afternoonDraw = [];
-    updateListsForAllClients(); // atualiza todos os clientes conectados
-
-    log("Todas as listas foram apagadas com sucesso.");
-    res.send("Listas apagadas. Pode voltar para a página principal.");
-  } catch (err) {
-    log("Erro ao apagar listas: " + err.message);
-    res.status(500).send("Erro ao apagar as listas. Tente novamente.");
-  }
-});
-
 let morningList = [];
 let afternoonList = [];
 let morningDraw = [];
@@ -322,6 +299,7 @@ async function runServer() {
 }
 
 runServer();
+
 
 
 
