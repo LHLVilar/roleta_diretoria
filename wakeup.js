4f958536c709109ff5d926a2d6a36b1da7918ce4
LHLVilar <101405552+LHLVilar@users.noreply.github.com>
2025-09-29 15:31:07 -0300

    Create wakeup.js
    wakeup Render

---
+++ wakeup.js
@@ -0,0 +1,28 @@
+import { io } from "socket.io-client";
+
+const URL = "https://roleta-diretoria-2.onrender.com/";
+
+function wakeUp() {
+  console.log("Tentando acordar app:", new Date().toISOString());
+
+  const socket = io(URL, {
+    transports: ["websocket"],
+    reconnection: false,
+    timeout: 5000,
+  });
+
+  socket.on("connect", () => {
+    console.log("Conectado com sucesso!");
+    setTimeout(() => {
+      socket.disconnect();
+      console.log("Desconectado após acordar o app.");
+    }, 3000);
+  });
+
+  socket.on("connect_error", (err) => {
+    console.error("Erro de conexão:", err.message);
+    process.exit(1);
+  });
+}
+
+wakeUp();
